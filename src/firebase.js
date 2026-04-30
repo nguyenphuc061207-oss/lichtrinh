182229149c73e22fab9101a63a335efb5f60ebdd
Nguyễn Ngọc Phúc <nguyenphuc061207@gmail.com>
2026-04-30 22:59:39 +0700

    Fix: Update valid Firebase API Key

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Dán đoạn config mà bạn lấy từ Firebase Console vào đây
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
+  apiKey: "AIzaSyADhDsfV9g2apcAghGA7PGuGbLJ4z9Cihk",
   authDomain: "lich-d694c.firebaseapp.com",
   projectId: "lich-d694c",
-  storageBucket: "lich-d694c.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  storageBucket: "lich-d694c.firebasestorage.app",
+  messagingSenderId: "816734434323",
+  appId: "1:816734434323:web:e8f00935e1eeddeda9fbe6",
+  measurementId: "G-KGV8HE92KJ"
 };
 
 // Khởi tạo Firebase
